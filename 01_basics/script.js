c5979f1ecaef3df87d5370589c34afaa84adea54
Rohitsingh Barad <101019277+rohitbarad@users.noreply.github.com>
2024-08-13 14:59:06 +0000

    Notes

--- 01_basics/script.js
+++ 01_basics/script.js
@@ -1,3 +1,26 @@
-'use strict';
+'use strict';// for newer version, a stricter use of code unlike older version
 
 console.log("Hello Rohit!");
+console.table(["hello!"]);
+
+
+console.log(typeof NaN); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+const name = function (){console.log("Rohit");}
+console.log(typeof name); // function, but its a function object
+
+
+let a = 3;
+let b = a++; //post
+console.log(`b: ${b} and a: ${a}`);
+
+a = 3;
+b = ++a;//pre
+console.log(`b: ${b} and a: ${a}`);
+
+
+// Primitive Values(number, string, boolean, BigInt, Symbol, undefined, null)
+// non-primitive or reference values(object, array, function, etc) all are objects
